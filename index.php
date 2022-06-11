e8d1e3bdd5a355f4a7b2e7a35ceea39c6ed60caf
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-06-11 18:36:22 +0200

    final version app

--- index.php
+++ index.php
@@ -2,10 +2,47 @@
 
 declare(strict_types=1);
 
-namespace App;
+// require_once('./src/Request.php');
 
-require('./src/utils/debug.php'); //dodaje link do folderu z plikiem
-require('./src/View.php');
+spl_autoload_register(function (string $name) {
+    $name = str_replace(['\\', 'App/'], ['/', ''], $name);
+    $path = "src/$name.php";
+    require_once($path);
+});
 
-$action = $GET['action'] ?? null;
-$view = new View();
+
+require_once('./src/utils/debug.php');
+$configuration = require_once('./config/config.php');
+
+use App\Request;
+use App\Controller\AbstractController;
+use App\Controller\NoteController;
+use App\Exception\AppException;
+use App\Exception\ConfigurationException;
+// use App\Request;
+
+
+
+// error_reporting(0);
+// ini_set('display_errors', '0');
+
+
+$request = new Request($_GET, $_POST, $_SERVER);
+
+
+
+try {
+    AbstractController::initConfiguration($configuration);
+    $controller = new NoteController($request);
+    $controller->run();
+} catch (ConfigurationException $e) {
+    echo "<h1>Wystąpił błąd aplikacji</h1>";
+    echo "<p>Błąd konfiguracji - skontaktuj się z administratorem xyz@gmail.com</p>";
+} catch (AppException $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+    echo '<h3>' . $e->getMessage() . '</h3>';
+    // echo '<h3>' . $e->getPrevious()->getMessage() . '</h3>';
+} catch (\Throwable $e) {
+    echo "<h1>Wystąpił błąd aplikacji!</h1>";
+    dump($e);
+}
--- templates/layout.php
+++ templates/layout.php
@@ -2,35 +2,38 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP NOTES</title>
+    <link rel="stylesheet" href="./public/style.css">
 </head>
 
 <body>
+    <header>
+        <h1>Moje notatki</h1>
+    </header>
 
-	<header>
-		<h1>Moje notatki</h1>
-	</header>
+    <main>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/notes">Lista notatek</a>
+                </li>
+                <li>
+                    <a href="/notes/?action=create">Nowa notatka</a>
+                </li>
+            </ul>
+        </nav>
 
-	<main>
-		<nav>
-			<ul>
-				<li>
-					<a href="/notes">Lista notatek</a>
-				</li>
-				<li>
-					<a href="/notes/?action=create">Nowa notatka</a>
-				</li>
-			</ul>
-		</nav>
-		<article>
-			...
-		</article>
-	</main>
-	<footer>Stopka</footer>
+        <article>
+            <?php
+            require_once("./templates/pages/$page.php");
+            ?>
+        </article>
+    </main>
 
+    <footer>Stopka</footer>
 </body>
 
 </html>
